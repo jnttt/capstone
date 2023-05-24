b3eb74d359a5edc817ed383c9d04036fd016339e
Jennifer <jennifer.ngt@gmail.com>
2023-05-24 14:54:22 -0500

    added more images

--- src/components/imagesData.js
+++ src/components/imagesData.js
@@ -4,6 +4,17 @@ const images = [
     { name: "02", img: "https://cdn.shibe.online/shibes/d3fce299fc5e7c9faac788936f24783828e6c9cc.jpg" },
     { name: "03", img: "https://cdn.shibe.online/shibes/81a65a81d4c03a5bc78bed9c27f3fd803b21286e.jpg" },
     { name: "04", img: "https://cdn.shibe.online/shibes/82d695cbcd0611c87e4292844d6cb1280d161ac7.jpg" },
+    { name: "05", img: "https://cdn.shibe.online/shibes/615311e22570aaaa6eb56bbf7e1b5fbd1a2cf3bc.jpg" },
+    { name: "06", img: "https://cdn.shibe.online/shibes/1243b3f8fbc715d0c7eb730ee44a97a842f1581e.jpg" },
+    { name: "07", img: "https://cdn.shibe.online/shibes/df1d55d6e222dbefa08c3163048a6af332aebb15.jpg" },
+    { name: "08", img: "https://cdn.shibe.online/shibes/e1088cbe3b47f17b4f8bf14d471d815838093518.jpg" },
+    { name: "09", img: "https://cdn.shibe.online/shibes/b70a5e33282870d0d73404064839204494fdc590.jpg" },
+    { name: "10", img: "https://cdn.shibe.online/shibes/3a794147112ca8e35fe45f41e84236655d89d678.jpg" },
+    { name: "11", img: "https://cdn.shibe.online/shibes/30daa0f8cc68fd671af38868edbc24bb63593f15.jpg" },
+    { name: "12", img: "https://cdn.shibe.online/shibes/60ab188df0311e17041a5b90583b0073abac6d92.jpg" },
+    { name: "13", img: "https://cdn.shibe.online/shibes/35512473ef581f496e3cccb111bb1a6d0c6ef4c2.jpg" },
+    { name: "14", img: "https://cdn.shibe.online/shibes/90e5142e263bb8da71a45f227408796f6804fc9c.jpg" },
+
   ];
   
   export default images;
